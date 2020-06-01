56cdc41f8ee62d873878bd5700106958d8766b35
warmike01 <51020399+warmike01@users.noreply.github.com>
2020-06-01 21:53:32 +0300

    Create main.cpp

---
+++ hw9/main.cpp
@@ -0,0 +1,32 @@
+//работает или нет, не знаю, попытка отладки намертво повесила винду на час
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+struct triangle
+{
+int a;
+int b;
+int c;
+double thickness;
+};
+const int TRIANGLE_SIZE=sizeof(triangle);
+int main()
+{
+    ifstream input;
+    input.open("triangle.bin",iostream::binary);
+   
+    if (!input.is_open())
+	{
+		cerr << "Ввод не открыт! "; return 1;
+    }
+    triangle find;
+    
+    int id;
+    cout << "Введите номер треугольника: "
+    cin >> id;
+    for(int i=0; i<id; i++){input.read(reinterpret_cast<triangle>(find), TRIANGLE_SIZE); }
+    printf("A= %d, B= %d, C= %d, thickness= %d",a,b,c,thickness);
+    return 0;
+}
+
